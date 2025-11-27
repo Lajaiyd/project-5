41b8becfc19f77a67dff5ed7a6ce22d328b9f59d
Olajide Akinyemi <iamjideakinyemi@gmail.com>
2025-11-26 19:41:40 -0500

    Project 5

--- ast/ast.hs
+++ ast/ast.hs
@@ -4,15 +4,23 @@ data Expr = Plus Expr Expr | Minus Expr Expr | Times Expr Expr | Div Expr Expr
     | Literal Float
 
 eval :: Expr -> Float
--- Your code goes here
+eval (Plus left right)  = eval left + eval right
+eval (Minus left right) = eval left - eval right
+eval (Times left right) = eval left * eval right
+eval (Div numerator denominator)   = eval numerator / eval denominator
+eval (Literal val)   = val
 
 eq :: Expr -> Expr -> Bool
--- Your code goes here
-
--- Should eval to "5.0"
+q (Literal x) (Literal y) = x == y
+eq (Plus a b) (Plus c d)       = eq a c && eq b d
+eq (Minus a b) (Minus c d)     = eq a c && eq b d
+eq (Times a b) (Times c d)     = eq a c && eq b d
+eq (Div a b) (Div c d)         = eq a c && eq b d
+eq _ _ = False
+-- Should eval to "5.0 "
 test1 = Plus (Literal 3.0) (Literal 2.0)
 
--- Should eval to "3.5"
+-- Should eval to "3.5 "
 test2 = Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
 
 -- Should eval to "15.5"
--- password/password.hs
+++ password/password.hs
@@ -1,15 +1,35 @@
 module Password where
 
-newtype PwdOp a = ???
+newtype PwdOp a = PwdOp { run :: String -> (a, String) }
+
+-- instance definitions for Functor, Applicative, and Monad
+instance Functor PwdOp where
+  fmap transform (PwdOp operation) = PwdOp $ \currentPwd ->
+      let (result, newPwd) = operation currentPwd
+      in (transform result, newPwd)
+
+instance Applicative PwdOp where
+  pure value = PwdOp $ \currentPwd -> (value, currentPwd)
+  (PwdOp funcOp) <*> (PwdOp valOp) = PwdOp $ \currentPwd ->
+      let (func, intermediatePwd) = funcOp currentPwd
+          (value, finalPwd) = valOp intermediatePwd
+      in (func value, finalPwd)
+
+instance Monad PwdOp where
+  return = pure
+  (PwdOp operation) >>= bindFunc = PwdOp $ \currentPwd ->
+      let (result, intermediatePwd) = operation currentPwd
+          PwdOp nextOperation = bindFunc result
+      in nextOperation intermediatePwd
 
 setPassword :: String -> PwdOp ()
--- Your code here
+setPassword newPwd = PwdOp $ \_ -> ((), newPwd)
 
 checkPassword :: String -> PwdOp Bool
--- Your code here
+checkPassword attempt = PwdOp $ \storedPwd -> (attempt == storedPwd, storedPwd)
 
 runPwdOp :: PwdOp a -> a
--- Your code here
+runPwdOp (PwdOp operation) = fst (operation "")
 
 
 
